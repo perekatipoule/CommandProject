83bf6a7f6fca36589ced43943d1ab467d2af06dd
Vova <mtyi@ukr.net>
2023-02-22 16:31:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/commandproject/ChatActivity.java
@@ -0,0 +1,19 @@
+package com.example.commandproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+}
---
+++ app/src/main/java/com/example/commandproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.commandproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
