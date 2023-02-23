4ad70264453f5cda2d22ebf4df71f2ab0f2029c7
Vova <mtyi@ukr.net>
2023-02-23 14:50:07 +0200

    some changer

--- app/src/main/java/com/example/commandproject/ChatActivity.java
+++ app/src/main/java/com/example/commandproject/ChatActivity.java
@@ -1,19 +1,34 @@
 package com.example.commandproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class ChatActivity extends AppCompatActivity {
-
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView backWord;
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        TextView contactName = findViewById(R.id.contactName);
+        TextView contactNumber = findViewById(R.id.contactNumber);
+        contactName.setText("Аркадій Вікторович Овдієнко");
+        contactNumber.setText("+380683812405");
+        backWord = findViewById(R.id.chat_back_word);
+        backButton = findViewById(R.id.chat_back_button);
+        backWord.setOnClickListener(this);
+        backButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.chat_back_word:
+            case R.id.chat_back_button:
+                finish();
+        }
     }
 }
